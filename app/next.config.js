0ff0d6426e43bc39d48751b8468c41f25a76c9ac
larbisahli <larbisahli1905@gmail.com>
2023-05-20 05:01:13 +0100

    remove test var

--- app/next.config.js
+++ app/next.config.js
@@ -3,7 +3,7 @@ const { i18n } = require('./next-i18next.config')
 const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
-  assetPrefix: !isProd ? 'https://cdn.dropgala.com' : undefined,
+  assetPrefix: isProd ? 'https://cdn.dropgala.com' : undefined,
   // output: 'standalone',
   eslint: {
     // Warning: This allows production builds to successfully complete even if
