1c24a348a0041c155a5b27869c8197d02a189873
larbisahli <larbisahli1905@gmail.com>
2024-12-02 12:40:47 +0100

    fix logo

--- packages/dropgala-ui/src/components/Logo/index.tsx
+++ packages/dropgala-ui/src/components/Logo/index.tsx
@@ -1,23 +1,30 @@
 import React from 'react'
-import { StoreProps } from '@dropgala/store'
+import { selectConfig, StoreProps } from '@dropgala/store'
 import Link from '../common/Link'
 import Image from '../common/Image'
+import { mediaURL } from '@dropgala/utils/utils'
 
-interface Props extends StoreProps {}
+interface Props extends StoreProps { }
 
 const Logo: React.FC<Props> = ({ useAppSelector, ...props }) => {
+  const storeConfig = useAppSelector(selectConfig)
+  const { device } = storeConfig
+
+  const storeLogo = !!storeConfig?.logo?.length
+    ? `${mediaURL}/${storeConfig?.logo[0].image}`
+    : '/assets/images/default_logo.webp'
+
   return (
     <div className="">
       <Link href="/">
-        {/* <Image
+        <Image
           isCustomUrl
-          src={'/'}
-          objectFit="contain"
-          height={30}
-          width={30}
+          src={storeLogo}
+          objectFit="cover"
+          height={device?.isDesktop ? 45 : 30}
+          width={device?.isDesktop ? 45 : 30}
           alt="logo"
-        /> */}
-        Logo
+        />
       </Link>
     </div>
   )
